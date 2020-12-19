8bef2e777e8b40e00b85d2c1d5c5f7e3dccfe01c
Tien Do <forever.love.wisdom@gmail.com>
2020-12-19 16:29:39 +0700

    Rubocop fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Shared helper methods for all views
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Abstract class for ActiveRecord
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
