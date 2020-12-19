488b9f3aa243f25efb738efbcc0414b170a4879b
Tien Do <forever.love.wisdom@gmail.com>
2020-12-19 17:12:37 +0700

    Add home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Handle routing request for Home
+class HomeController < ApplicationController
+  def news; end
+end
---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Shared helpers for Home views
+module HomeHelper
+end
