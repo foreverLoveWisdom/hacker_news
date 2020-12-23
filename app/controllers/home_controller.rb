94628fa1502ef33e2a67c3899017ad4a6244adf0
Tien Do <forever.love.wisdom@gmail.com>
2020-12-23 11:20:10 +0700

    Load only url and title on backend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@
 # Handle routing request for Home
 class HomeController < ApplicationController
   def news
+    binding.pry
     @news_content = NewsParsing.new.parse
   end
 end
--- app/lib/news_parsing.rb
+++ app/lib/news_parsing.rb
@@ -9,6 +9,7 @@ class NewsParsing
 
   def initialize(url = HACKER_NEWS)
     @url = url
+    @news_list = []
   end
 
   def parse
@@ -17,7 +18,18 @@ class NewsParsing
       response = Net::HTTP.get_response(uri)
       raise StandardError, "Failed to fetch page content. Response code: #{response.code}" unless response.code == '200'
 
-      generate_news_list(response)
+      doc = Nokogiri::HTML(response.body)
+      story_links = doc.xpath("//table[1]/tr/td/a[@class='storylink']")
+      news_list = []
+      story_links.each do |story_link|
+      article_url = story_link.attributes['href'].to_s
+      news_hash = {
+        title: story_link.text,
+        article_url: article_url,
+      }
+      news_list << news_hash
+      end
+      news_list
     rescue StandardError => e
       Rails.logger.debug { "Rescued exception: #{e.inspect}" }
     end
@@ -25,49 +37,10 @@ class NewsParsing
 
   private
 
-  def generate_news_list(response)
-    best_news = []
-    doc = Nokogiri::HTML(response.body)
-    story_links = doc.xpath("//table[1]/tr/td/a[@class='storylink']")
-    fetch_articles_info(best_news, story_links)
-    best_news
-  end
-
-  def fetch_articles_info(best_news, story_links)
-    story_links.each do |story_link|
-      extract_article_data(best_news, story_link)
-    rescue StandardError => e
-      Rails.logger.debug { "Rescued exception: #{e.inspect}" }
-    ensure
-      next
-    end
-  end
-
-  def extract_article_data(best_news, story_link)
-    article_uri = URI(story_link.attributes['href'].to_s)
-    article_response = Net::HTTP.get_response(article_uri)
-    unless article_response.code == '200'
-      raise StandardError,
-            "Failed to fetch article content at #{article_uri}. Response code: #{article_response.code}"
-    end
-
-    article_body = filter_article_content(article_response)
-    news_hash = get_news_info(article_body, story_link)
-    best_news << news_hash
-  end
-
-  def get_news_info(article_body, story_link)
-    {
-      title: story_link.text,
-      image_url: article_body.images.first,
-      article_excerpt: "#{article_body.content.strip.first(50)}...".gsub(/\n/, '')
-    }
-  end
-
-  def filter_article_content(article_response)
-    Readability::Document.new(article_response.body,
-                              tags: %w[p img],
-                              attributes: %w[src href],
-                              remove_empty_nodes: true)
-  end
+  # def filter_article_content(article_response)
+  #   Readability::Document.new(article_response.body,
+  #                             tags: %w[p img],
+  #                             attributes: %w[src href],
+  #                             remove_empty_nodes: true)
+  # end
 end
